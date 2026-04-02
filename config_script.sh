b59ecbde3c30f025af0c66ca48cce15da31cb6b3
Rodrigo Soares Ferreira <92071020+Atreus75@users.noreply.github.com>
2026-04-02 10:37:57 -0300

    Improve VIM setup script with checks and sudo
    Refactor script to check for existing directories and files before creating or moving. Change apt install commands to use sudo and add user prompts for Vim plugin installation.

--- config_script.sh
+++ config_script.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
 
 echo "[+] Starting VIM basic configuration for Python and C programming";
-if [ "root" -ne whoami ]; then
-  echo "[-] You need to run it as root";
-  echo "[-] Exiting";
-  exit;
-fi
 echo "[+] Clonning VIM-Plug";
 git clone https://github.com/junegunn/vim-plug.git 1>/dev/null;
 echo "[+] Changing VIM config files";
-mkdir ~/.vim 2>/dev/null;
-mkdir ~/.vim/autoload 2>/dev/null;
-mv vim-plug/plug.vim ~/.vim/autoload/plug.vim;
-mv vimrc > ~/.vimrc;
+if ! test -d ~/.vim; then
+	mkdir ~/.vim;
+	mkdir ~/.vim/autoload;
+fi
+if ! test -f ~/.vim/autoload/plug.vim; then
+	mv vim-plug/plug.vim ~/.vim/autoload/plug.vim;
+fi
+mv vimrc ~/.vimrc;
 echo "[+] Installing Python and C LSP";
-apt install build-essential cmake vim-nox python3-dev;
-apt install mono-complete golang nodejs openjdk-17-jdk openjdk-17-jre npm;
-vim -c ":PlugInstall" none;
-cd ~/.vim/plugged/YouCompleteMe;
-python3 install.py --clangd-completer;
+sudo apt install build-essential cmake vim-nox python3-dev;
+sudo apt install mono-complete golang nodejs openjdk-17-jdk openjdk-17-jre npm;
+read -p "Type :PlugInstall inside vim, wait the end of the plugins installation, and exit with :q";
+vim none;
+python3 ~/.vim/plugged/YouCompleteMe/install.py --clangd-completer;
 echo "[+] Completed";
