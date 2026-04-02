9195306f5d45bac32bae48b5f562dcf03044b153
Rodrigo Soares Ferreira <92071020+Atreus75@users.noreply.github.com>
2026-04-02 10:08:34 -0300

    Enhance VIM configuration script with user checks
    Added checks for root user and improved output messages during VIM configuration setup.

--- config_script.sh
+++ config_script.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
 
-git clone https://github.com/Atreus75/vim_configurations.git;
-git clone https://github.com/junegunn/vim-plug.git
-mv vim-plug/plug.vim ~/.vim/autoload/plug.vim
-cd ../;
-vim -c ":PlugInstall" -c ":q!" none;
-cd vim_configurations;
-cat vimrc > ~/.vimrc;
+echo "[+] Starting VIM basic configuration for Python and C programming";
+if [ "root" -ne whoami ]; then
+  echo "[-] You need to run it as root";
+  echo "[-] Exiting";
+  exit;
+fi
+echo "[+] Clonning VIM-Plug";
+git clone https://github.com/junegunn/vim-plug.git 1>/dev/null;
+echo "[+] Changing VIM config files";
+mkdir ~/.vim 2>/dev/null;
+mkdir ~/.vim/autoload 2>/dev/null;
+mv vim-plug/plug.vim ~/.vim/autoload/plug.vim;
+mv vimrc > ~/.vimrc;
+echo "[+] Installing Python and C LSP";
+apt install build-essential cmake vim-nox python3-dev;
+apt install mono-complete golang nodejs openjdk-17-jdk openjdk-17-jre npm;
+vim -c ":PlugInstall" none;
+cd ~/.vim/plugged/YouCompleteMe;
+python3 install.py --clangd-completer;
+echo "[+] Completed";
